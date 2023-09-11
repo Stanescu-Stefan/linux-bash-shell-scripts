8bf95bde921fbb751df5090b49837bf461a2189b
Stanescu Stefan <stanescu.irinel.stefan@gmail.com>
2023-09-11 21:39:56 +0300

    added disk utilization check

--- resource_mail_alert.sh
+++ resource_mail_alert.sh
@@ -7,9 +7,19 @@ THRESHOLD=400
 #get free RAM value
 FREE_RAM=$(free -mt | grep -E "Total" | awk '{print $4}')
 
+#check if free ram value is below the threshold and send emai about it
 if [[ $FREE_RAM -lt $THRESHOLD ]]
 then
-  echo -e "Server is running low on RAM memory\n Available RAM is: $FREE_RAM" | mail -s "RAM INFO $(date)" $TO
+  echo -e "Server is running low on RAM memory\n Available RAM is: $FREE_RAM" | /bin/mail -s "RAM INFO $(date)" $TO
 fi
 
-#you can create a cronjob to run this script every second to monitor the resources
+
+#we can also check disk utilization if its above a certain value and send a notification about it
+FILE_UTILIZATION=$(du -H | grep /grep/xvda | awk '{ print $5 }' | tr -d "%")
+if [[ $FILE_UTILIZATION -ge 55 ]]
+then
+  echo "The filesystem /dev/xvda usage is: $FILE_UTILIZATION" | /bin/mail -s "Filesystem Utilization alert" $TO
+fi
+
+
+#you can create a cronjob to run this script every second/hour/certain hoursto monitor the resources depending on your needs
